bde5587f4f5abcc76eac0e12dfbab3951e6170e5
Julien Dargelos <julien.dargelos@me.com>
2017-10-01 03:44:22 +0200

    Improvements wip

--- src/index.js
+++ src/index.js
@@ -1,100 +1,139 @@
 export default {
-  computed: {
-    is () {
-      return Object.create({}, this.modifiersDescriptor)
-    },
-
-    own () {
-      return this.is
-    },
-
-    modifiersDescriptor () {
-      var descriptor = {}
-      for (var modifier in this.modifiers) descriptor[modifier] = this.modifierDescriptor(modifier)
-
-      return descriptor
-    }
-  },
-
   mounted () {
-    this.initModifiers()
+    this.initBem()
   },
 
   methods: {
-    initModifiers () {
-      for(var modifier in this.modifiers) {
-        this.setModifier(modifier, this[modifier] === undefined ? this.modifiers[modifier] : this[modifier])
+    initBem () {
+      if (typeof this.bem !== 'object' || this.bem === null) return
+
+      if (typeof this.bem.modifiers === 'object' && this.bem.modidifers !== null) {
+        this.setModifiersDescriptor(this, this.$el, this.bem.name, this.bem.modifiers)
+      }
+
+      if (typeof this.bem.elements === 'object' && this.bem.elements !== null) {
+        this.setElementsDescriptor(this, this.$el, this.bem.name, this.bem.elements)
       }
     },
 
-    modifierDescriptor (modifier) {
-      return {
-        get: () => this.getModifier(modifier),
-        set: value => this.setModifier(modifier, value)
+    setElementsDescriptor (object, parent, block, elements) {
+      var override = {}
+
+      for (var property in object) {
+        if (property.match(/^is[A-Z]/) !== null) {
+          override[property.substring(2, 1).toLowerCase() + property.substring(4)] = object[property]
+        }
+      }
+
+      for (var name in elements) {
+        object[name] = {}
+        this.setModifiersDescriptor(
+          object[name],
+          () => parent.querySelector('.' + this.elementString(block, name)),
+          this.elementString(block, name),
+          elements[name],
+          override
+        )
       }
     },
 
-    getModifier (modifier) {
-      if(this.modifierType (modifier) === 'boolean') return this.getBooleanModifier(modifier)
-      else return this.getStringModifier(modifier)
+    elementString (block, name) {
+      return block + '__' + name
     },
 
-    setModifier (modifier, value) {
-      if(this.modifierType (modifier) === 'boolean') return this.setBooleanModifier(modifier, value)
-      else return this.setStringModifier(modifier, value)
+    setModifiersDescriptor (object, node, name, modifiers, override) {
+      override = Object.Assign({}, override || object)
+      Object.defineProperties(object, this.modifiersDescriptor(node, name, modifiers, override))
+
+      for(var modifier in modifiers) {
+        object[modifier] = override[modifier] === undefined ? modifiers[modifier] : override[modifier]
+      }
     },
 
-    modifierType(modifier) {
-      return this.modifierValueType(this.modifiers[modifier])
+    modifiersDescriptor  (node, name, modifiers, override) {
+      var descriptor = {}
+
+      for (var modifier in modifiers) {
+        descriptor[modifier] = this.modifierDescriptor(node, name, modifier, modifiers[modifier])
+      }
+
+      return descriptor
+    },
+
+    modifierDescriptor (node, name, modifier, base) {
+      if (this.modifierValueType(base) === 'boolean') {
+        return this.booleanModifierDescriptor(node, name, modifier)
+      } else {
+        return this.stringModifierDescriptor(node, name, modifier)
+      }
     },
 
     modifierValueType (value) {
       return [true, false].includes(value) ? 'boolean' : 'string'
     },
 
-    booleanModifierString (modifier) {
-      return this.bem + '--' + modifier
-    },
-
-    booleanModifierPattern (modifier) {
-      return new RegExp('\\b' + this.booleanModifierString(modifier) + '\\b')
-    },
-
-    getBooleanModifier (modifier) {
-      if (!this.$el) return false
-      return this.$el.className.match(this.booleanModifierPattern(modifier)) !== null
-    },
-
-    setBooleanModifier (modifier, value) {
-      if (!this.$el) return
-
-      value = !!value
-
-      if(this.getBooleanModifier(modifier) !== value) {
-        if(value) this.$el.className += ' '+this.booleanModifierString(modifier)
-        else this.$el.className = this.$el.className.replace(this.booleanModifierPattern(modifier), '').trim()
+    booleanModifierDescriptor (node, name, modifier) {
+      return {
+        get: () => this.getBooleanModifier(node, modifier),
+        set: value => this.setBooleanModifier(node, modifier, value)
       }
     },
 
-    stringModifierString (modifier, value) {
-      return this.bem + '--' + modifier + '--' + value
+    booleanModifierString (name, modifier) {
+      return name + '--' + modifier
     },
 
-    stringModifierPattern (modifier) {
-      return new RegExp('\\b' + this.stringModifierString(modifier, '([^\\s]+)') + '\\b')
+    booleanModifierPattern (name, modifier) {
+      return new RegExp('\\b' + this.booleanModifierString(name, modifier) + '\\b')
     },
 
-    getStringModifier (modifier) {
-      if (!this.$el) return null
-      return (this.$el.className.match(this.stringModifierPattern(modifier)) || [null, null])[1]
+    getBooleanModifier (node, name, modifier) {
+      if (!node) return false
+      if(typeof node === 'function') node = node()
+
+      return node ? node.className.match(this.booleanModifierPattern(name, modifier)) !== null : false
     },
 
-    setStringModifier (modifier, value) {
-      if (!this.$el) return
+    setBooleanModifier (node, name, modifier, value) {
+      if (!node) return
+      if(typeof node === 'function') node = node()
+      value = !!value
 
-      if (this.getStringModifier(modifier) !== value) {
-        this.$el.className = this.$el.className.replace(this.stringModifierPattern(modifier), '').trim()
-        if (value) this.$el.className += ' '+this.stringModifierString(modifier, value)
+      if(node && this.getBooleanModifier(node, name, modifier) !== value) {
+        if(value) node.className += ' '+this.booleanModifierString(name, modifier)
+        else node.className = node.className.replace(this.booleanModifierPattern(name, modifier), '').trim()
+      }
+    },
+
+    stringModifierDescriptor (node, name, modifier) {
+      return {
+        get: () => this.getStringModifier(node, modifier),
+        set: value => this.setStringModifier(node, modifier, value)
+      }
+    },
+
+    stringModifierString (name, modifier, value) {
+      return name + '--' + modifier + '--' + value
+    },
+
+    stringModifierPattern (name, modifier) {
+      return new RegExp('\\b' + this.stringModifierString(name, modifier, '([^\\s]+)') + '\\b')
+    },
+
+    getStringModifier (node, name, modifier) {
+      if (!node) return null
+      if(typeof node === 'function') node = node()
+
+      return node ? (node.className.match(this.stringModifierPattern(name, modifier)) || [null, null])[1] : null
+    },
+
+    setStringModifier (node, name, modifier, value) {
+      if (!node) return
+      if(typeof node === 'function') node = node()
+
+      if (node && this.getStringModifier(node, name, modifier) !== value) {
+        node.className = node.className.replace(this.stringModifierPattern(name, modifier), '').trim()
+        if (value) node.className += ' '+this.stringModifierString(name, modifier, value)
       }
     }
   }
